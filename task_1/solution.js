7385ea54d0491df9af9a43a60c8a8ff6347739dc
valerika220194 <valeriya.karelova1994@gmail.com>
2026-03-03 16:27:01 +0300

    Add files via upload

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,11 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = title + " " + "за" + " " + "price" + " " + "теперь в корзине"
+    console. log (message)
+
+
+
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +28,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue + "1";
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +43,10 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference
+    let newSumText = newSum + "₽"
+    newSumText = `${newSum} ₽`
+
 
     // Задание №1.3. Увеличение суммы в корзине
 
